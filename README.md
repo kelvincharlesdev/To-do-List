d8c810220f9d2396105c68de7f59544d87dd7ccc
Kelvin Charles <kelvinc1291@hotmail.com>
2023-02-15 18:22:31 -0300

    Primeiro commit, projeto concluido com todos arquivos

---
+++ README.md
@@ -0,0 +1,50 @@
+# 🖥️ Projeto To-Do-List da comunidade Desbravando a Programação
+
+## Descrição
+
+Projeto realizado com colaboração, a fim de treinar trabalho em equipe e trocar conhecimentos e experiências! 
+
+## ✍️ Processo
+
+    - Marcação semântica HTML5
+    - Propriedades personalizadas de CSS3
+    - Flexbox
+    - Design responsivo
+    - JavaScript
+
+## ⚙ Funcionalidades
+
+    - Criação de novas tarefas 
+    - Edição de tarefas criadas 
+    - Marcação de tarefas concluídas 
+    - Exclusão de tarefas 
+
+>Acesse a página on-line: <a href="https://to-do-list-8xis4z7xn-andersonrodrigs.vercel.app/?" target=_blank> To Do List </a>
+
+
+🖱️ A página
+<img src="src/design/to-do-list.png" alt="Imagem exibindo a versão desktop  do site">
+
+👩‍💻 Dev's
+
+<table align="center">
+    <tr>   
+      <td align="center">
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/105132452?v=4" width="120px;" alt="Foto de Jean no GitHub"/><br>
+                <b> Anderson Rodrigues </b><br>
+                <a href="https://www.linkedin.com/in/anderson-rodrigues-s "alt="Linkedin"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="20"></a>
+                <a href="https://github.com/AndersonRodrigs" alt="GitHub"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="20"></a>
+            </div>
+        </td>
+        <td align="center">
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/110488969?v=4" width="120px;" alt="Foto de Kelvin no GitHub"/><br>
+                <b> Kelvin Charles </b><br>
+                <a href="https://www.linkedin.com/in/kelvin-charles/" alt="Linkedin"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="20"></a>
+                <a href="https://github.com/kelvincharlesdev" alt="GitHub"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="20"></a>
+            </div>
+        </td>
+    </tr>
+</table>
+
---
+++ src/js/index.js
@@ -0,0 +1,110 @@
+const tarefas = document.getElementById('tarefas')
+let contador = 0
+const numTarefas = document.querySelector('.num-tarefas')
+
+function adicionarTarefa() {
+  const input = document.getElementById('tarefa-digitada')
+
+  if (input.value !== '') {
+    input.removeAttribute('required', 'required')
+    contador++
+    console.log(contador)
+
+    input.classList.remove('vazio')
+    const idTarefa = input.value.charAt(0) + Math.floor(Math.random() * 1000)
+
+    const li = document.createElement('li')
+    li.setAttribute('id', idTarefa)
+
+    li.setAttribute('class', 'tarefa')
+
+    const titulo = document.createElement('h3')
+    titulo.innerText = input.value
+
+    const btns = document.createElement('div')
+    btns.setAttribute('class', 'botoes')
+
+    const [btnRemover, btnEditar, btnConcluir] = botoes(idTarefa)
+
+    btns.append(btnRemover, btnEditar, btnConcluir)
+
+    li.appendChild(titulo)
+
+    li.appendChild(btns)
+
+    tarefas.appendChild(li)
+
+    input.value = ''
+  } else {
+    input.classList.add('vazio')
+    input.setAttribute('required', 'required')
+  }
+
+  numTarefas.innerHTML = contador
+}
+
+function botoes(idTarefa) {
+  const btnRemover = document.createElement('button')
+  btnRemover.setAttribute('class', 'remover')
+  btnRemover.innerHTML = '<i class="fas fa-trash-alt"></i>'
+  btnRemover.setAttribute('onclick', `remover(${idTarefa})`)
+
+  const btnEditar = document.createElement('button')
+  btnEditar.setAttribute('class', 'editar')
+  btnEditar.innerHTML = ' <i class="far fa-edit"></i>'
+  btnEditar.setAttribute('onclick', `editar(${idTarefa})`)
+
+  const btnConcluir = document.createElement('button')
+  btnConcluir.setAttribute('class', 'concluir')
+  btnConcluir.innerHTML = '<i class="far fa-check-circle"></i>'
+  btnConcluir.setAttribute('onclick', `concluir(${idTarefa})`)
+
+  return [btnRemover, btnEditar, btnConcluir]
+}
+
+let contadorConcluidas = 0
+let tarefasConcluidas = document.querySelector('.tarefas-concluidas')
+
+function remover(idTarefa) {
+  tarefas.removeChild(idTarefa)
+  contador--
+
+  numTarefas.innerHTML = contador
+
+  if (contadorConcluidas > 0) {
+    contadorConcluidas--
+    tarefasConcluidas.innerHTML = contadorConcluidas
+  }
+}
+
+function editar(idTarefa) {
+  if (idTarefa.classList.contains('tarefa-concluida')) {
+    return
+  }
+
+  const tarefaEditada = prompt('Edite sua tarefa')
+  if (tarefaEditada != null) {
+    idTarefa.firstChild.innerHTML = tarefaEditada
+  }
+}
+
+function concluir(idTarefa) {
+  if (idTarefa.classList.contains('tarefa-concluida')) {
+    idTarefa.classList.remove('tarefa-concluida')
+    contadorConcluidas--
+  } else {
+    idTarefa.classList.add('tarefa-concluida')
+    contadorConcluidas++
+  }
+  tarefasConcluidas.innerHTML = contadorConcluidas
+}
+
+const form = document.querySelector('form')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+})
+
+const btn = document.querySelector('.adicionar-tarefa')
+btn.addEventListener('click', e => {
+  adicionarTarefa()
+})
